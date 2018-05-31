c507944817b232c03380728511e7546740933008
Helge Heß <devteam@zeezide.de>
2018-05-31 14:51:09 +0200

    Work on demo app
    ... hyperlink sample.
    dateformatter.

--- Sources/WOShowcaseApp/Components/DemoWOHyperlink.swift
+++ Sources/WOShowcaseApp/Components/DemoWOHyperlink.swift
@@ -10,10 +10,31 @@ import SwiftObjects
 
 final class DemoWOHyperlink : WOComponent {
   
-  let fourtyTwo = 42
-  let pi        = 3.1415
-  let hello     = "Hello World"
-  let now       = Date()
-  let empty     = ""
+  var counter = 0
   
+  let bindingInfo = // for display :-) If only we had reflection ;-)
+  """
+  var counter = 0
+  """.trimmingCharacters(in: .whitespacesAndNewlines)
+  
+  override func awake() {
+    super.awake()
+    
+    expose(increment, as: "increment")
+    expose(decrement, as: "decrement")
+    expose(doDouble,  as: "double")
+  }
+  
+  func increment() -> Any {
+    counter += 1
+    return self
+  }
+  func decrement() -> Any {
+    counter -= 1
+    return self
+  }
+  func doDouble() -> Any {
+    counter *= 2
+    return self
+  }
 }

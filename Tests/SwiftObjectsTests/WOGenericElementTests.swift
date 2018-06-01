2bf1b6632a6b9990efb0edd1c617c31d64094599
Helge Heß <devteam@zeezide.de>
2018-06-01 14:53:15 +0200

    Test for generic element rendering

--- Tests/SwiftObjectsTests/WOGenericElementTests.swift
+++ Tests/SwiftObjectsTests/WOGenericElementTests.swift
@@ -5,4 +5,33 @@
 //  Created by Helge Hess on 01.06.18.
 //
 
-import Foundation
+import XCTest
+@testable import SwiftObjects
+
+class WOGenericElementTests: DynamicElementTestCase {
+  
+  func testLIWithClass() throws {
+    // Note: This is intentionally wrong! "isSelected" is a constant assoc
+    //       here! (which happens to evaluate to "true" :-) )
+    
+    // <#li .selected="isSelected"><wo:str value="10" /></#li>
+    var bindings : [ String : WOAssociation ] = [
+      "elementName" : WOAssociationFactory.associationWithValue("li"),
+      ".selected"   : WOAssociationFactory.associationWithValue("isSelected")
+    ]
+    let t = WOString(value: WOValueAssociation(10))
+    let e = WOGenericContainer(name: "TestGC", bindings: &bindings, template: t)
+    XCTAssert(bindings.isEmpty, "element did not consume all bindings")
+    
+    try e.append(to: response, in: context)
+    XCTAssertEqual(response.contentString, "<li class=\"selected\">10</li>")
+  }
+
+  
+  // MARK: - Linux
+  
+  static var allTests = [
+    ( "testLIWithClass", testLIWithClass ),
+  ]
+}
+

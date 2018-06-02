59b032bf5ffc582b692a41f4d8d5f82092ed5b18
Helge Heß <devteam@zeezide.de>
2018-06-02 14:20:59 +0200

    Map names of new elements
    ... sigh.

--- Sources/SwiftObjects/Elements/WOElementNames.swift
+++ Sources/SwiftObjects/Elements/WOElementNames.swift
@@ -10,41 +10,60 @@ let WOElementNames : [ String : WODynamicElement.Type ] = {
   map.reserveCapacity(64)
   map.merge(WOBasicElementNames, uniquingKeysWith: { first, _ in first })
   map.merge(WOFormElementNames,  uniquingKeysWith: { first, _ in first })
+  map.merge(WOLinkElementNames,  uniquingKeysWith: { first, _ in first })
   map.merge(WOMiscElementNames,  uniquingKeysWith: { first, _ in first })
   return map
 }()
 
-let WOBasicElementNames : [ String : WODynamicElement.Type ] = [
-  "WOConditional"      : WOConditional.self,
-  "WOGenericElement"   : WOGenericElement.self,
-  "WOGenericContainer" : WOGenericContainer.self,
-  "WOHyperlink"        : WOHyperlink.self,
-  "WOImage"            : WOImage.self,
-  "WORepetition"       : WORepetition.self,
-  "WOString"           : WOString.self,
-  "WOComponentContent" : WOComponentContent.self,
-  "WOFragment"         : WOFragment.self
+let WOBasicElementNames  : [ String : WODynamicElement.Type ] = [
+  "WOComponentContent"   : WOComponentContent.self,
+  "WOConditional"        : WOConditional.self,
+  "WOFragment"           : WOFragment.self,
+  "WOGenericContainer"   : WOGenericContainer.self,
+  "WOGenericElement"     : WOGenericElement.self,
+  "WOHyperlink"          : WOHyperlink.self,
+  "WOImage"              : WOImage.self,
+  "WORepetition"         : WORepetition.self,
+  "WOString"             : WOString.self,
+  "WOSwitchComponent"    : WOSwitchComponent.self,
 ]
 
-let WOFormElementNames : [ String : WODynamicElement.Type ] = [
-  "WOCheckBox"         : WOCheckBox.self,
-  "WOForm"             : WOForm.self,
-  "WOHiddenField"      : WOHiddenField.self,
-  "WOInput"            : WOInput.self,
-  "WOResetButton"      : WOResetButton.self,
-  "WOSearchField"      : WOSearchField.self,
-  "WOSubmitButton"     : WOSubmitButton.self,
-  "WOText"             : WOText.self,
-  "WOTextField"        : WOTextField.self
+let WOFormElementNames   : [ String : WODynamicElement.Type ] = [
+  "WOCheckBox"           : WOCheckBox.self,
+  "WOForm"               : WOForm.self,
+  "WOHiddenField"        : WOHiddenField.self,
+  "WOInput"              : WOInput.self,
+  "WOResetButton"        : WOResetButton.self,
+  "WOSearchField"        : WOSearchField.self,
+  "WOSubmitButton"       : WOSubmitButton.self,
+  "WOText"               : WOText.self,
+  "WOTextField"          : WOTextField.self,
+  "WOPopUpButton"        : WOPopUpButton.self,
+  "WORadioButton"        : WORadioButton.self,
+  "WOPasswordField"      : WOPasswordField.self,
 ]
 
-let WOMiscElementNames : [ String : WODynamicElement.Type ] = [
+let WOLinkElementNames   : [ String : WODynamicElement.Type ] = [
+  "WOActionURL"          : WOActionURL.self,
+  "WOResourceURL"        : WOResourceURL.self,
+]
+
+let WOMiscElementNames   : [ String : WODynamicElement.Type ] = [
   "WOConditionalComment" : WOConditionalComment.self,
   "WOEntity"             : WOEntity.self,
   "WOJavaScript"         : WOJavaScript.self,
   "WOParam"              : WOParam.self,
-  "WOResourceURL"        : WOResourceURL.self,
   "WOSetHeader"          : WOSetHeader.self,
   "WOStylesheet"         : WOStylesheet.self,
-  "WOXmlPreamble"        : WOXmlPreamble.self
+  "WOXmlPreamble"        : WOXmlPreamble.self,
+  "WOBody"               : WOBody.self,
+  "WOFrame"              : WOFrame.self,
+  "WOIFrame"             : WOIFrame.self,
+  "WOHtml"               : WOHtml.self,
+]
+
+let WOFormatterNames     : [ String : WOFormatter.Type ] = [
+  "WODateFormatter"      : WODateFormatter.self,
+  "WONumberFormatter"    : WONumberFormatter.self,
+  "WOObjectFormatter"    : WOObjectFormatter.self,
 ]

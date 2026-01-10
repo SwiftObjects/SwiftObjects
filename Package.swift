14bdd07ddd592dc022133d10e8d1d59e2e87fba0
Helge Heß <devteam@zeezide.de>
2026-01-10 13:21:14 +0100

    Use the latest libs
    ...

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 //  SwiftObjects
 //
 //  Created by Helge Hess on 11.05.18.
-//  Copyright © 2018-2020 ZeeZide. All rights reserved.
+//  Copyright © 2018-2026 ZeeZide. All rights reserved.
 //
 import PackageDescription
 
@@ -17,15 +17,15 @@ let package = Package(
 
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git",
-                 from: "2.25.1"),
+                 from: "2.92.1"),
         .package(url: "https://github.com/wickwirew/Runtime.git",
-                 from: "2.2.2"),
+                 from: "2.2.7"),
         
         // just for the showcase
         .package(url: "https://github.com/SwiftWebResources/SemanticUI-Swift.git",
-                 from: "2.4.2"),
+                 from: "2.5.0"),
         .package(url: "https://github.com/SwiftWebResources/jQuery-Swift.git",
-                 from: "3.5.1"),
+                 from: "3.7.1"),
     ],
 
     targets: [
